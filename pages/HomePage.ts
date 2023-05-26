e516e21b096d54af56f9ef5573b829f889ffb18c
Behery <elbehery.muhammed@gmail.com>
2023-05-27 02:01:04 +0300

    finishing up signup test

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -3,12 +3,39 @@ import type { Locator , Page } from  '@playwright/test';
 export class HomePage {
 
     readonly page: Page
-    readonly newUserBtn: Locator;
+    readonly homePageHeader: Locator
+    readonly newUserBtn: Locator
+    readonly continueBtn: Locator
+    readonly enterEmailInput: Locator
+    readonly submitBtn: Locator
+    readonly phoneExtensionInput: Locator
+    readonly phoneNumberInput: Locator
+    readonly enterNameInput: Locator
+    readonly enterPasswordInput: Locator
+    readonly signUpSuccessMessage: Locator
+    readonly signUpPageLoginBtn: Locator
+    readonly loginPageHeader: Locator
+    readonly returningUserBtn: Locator
+    readonly fastTrackCrmHeader: Locator
 
     constructor(page:Page){
-      
+
         this.page = page
+        this.homePageHeader = page.locator('//h1[contains(text(),"Start your full tour here!")]')
         this.newUserBtn = page.locator('//button[contains(text(),"New user")]')
+        this.continueBtn = page.locator('//button[contains(text(),"I GET IT, CONTINUE")]')
+        this.enterEmailInput = page.locator('//input[@placeholder="Enter Email"]')
+        this.submitBtn = page.locator('//button[@type="submit"]')
+        this.phoneExtensionInput = page.locator('//input[@placeholder="+46"]')
+        this.phoneNumberInput = page.locator('//input[@placeholder="Phone number"]')
+        this.enterNameInput = page.locator('//input[@placeholder="Enter your name"]')
+        this.enterPasswordInput = page.locator('//input[@placeholder="Password"]')
+        this.signUpSuccessMessage = page.locator('//h3[contains(text(),"Your registration is complete")]')
+        this.signUpPageLoginBtn = page.locator('//button[contains(text(),"Login")]')
+        this.loginPageHeader = page.locator('//h1[contains(text(),"Login")]')
+        this.returningUserBtn = page.locator('//button[contains(text(),"Returning user")]')
+        this.fastTrackCrmHeader = page.locator('//h1[contains(text(),"Fast Track CRM")]')
+
     }
 
     async navigateToHomePage(){
@@ -17,9 +44,57 @@ export class HomePage {
 
     }
 
-    async register(){
+    async generateRandomEmail(){
+
+      let rand = Math.random() * 99999
+
+      rand = Math.floor(rand)
+
+      let randomEmail = 'elbehery.muhammed+test_' + rand + '@gmail.com'
+
+      return randomEmail
+
+    }
+
+    async register(newEmail: string, newPhoneExtension: string, newPhoneNumber: string, newName: string, newPassword: string){
 
       await this.newUserBtn.click()
+
+      await this.continueBtn.click()
+
+      await this.enterEmailInput.fill(newEmail)
+
+      await this.submitBtn.click()
+
+      await this.phoneExtensionInput.fill(newPhoneExtension)
+
+      await this.phoneNumberInput.fill(newPhoneNumber)
+
+      await this.submitBtn.click()
+
+      await this.enterNameInput.fill(newName)
+
+      await this.submitBtn.click()
+
+      await this.enterPasswordInput.fill(newPassword)
+
+      await this.submitBtn.click()
+
+   }
+
+   async loginFromSignUpPage(){
+
+      await this.signUpPageLoginBtn.click()
+
+   }
+
+   async loginFromHomePage(userEmail: string){
+
+      await this.returningUserBtn.click()
+
+      await this.enterEmailInput.fill(userEmail)
+
+      await this.submitBtn.click()
 
    }
     
--- tests/TC001_SignUp.spec.ts
+++ tests/TC001_SignUp.spec.ts
@@ -6,10 +6,33 @@ test('Should be able to sign up', async ({ page }) => {
   
     const app = new App(page)
 
+    const newEmail = await app.HomePage.generateRandomEmail()
+
+    const newPhoneExtension = "+20"
+
+    const newPhoneNumber = "1067779827"
+
+    const newName = "Test Account"
+
+    const newPassword = "Nopassword@123"
+
     await app.HomePage.navigateToHomePage()
 
-    await app.HomePage.register()
+    await expect(app.HomePage.homePageHeader).toBeVisible()
 
+    await app.HomePage.register(newEmail, newPhoneExtension, newPhoneNumber, newName, newPassword)
+
+    await expect(app.HomePage.signUpSuccessMessage).toBeVisible()
+
+    await app.HomePage.loginFromSignUpPage()
+
+    await expect(app.HomePage.loginPageHeader).toBeVisible()
+
+    await app.HomePage.navigateToHomePage()
+
+    await app.HomePage.loginFromHomePage(newEmail)
+
+    await expect(app.HomePage.fastTrackCrmHeader).toBeVisible
 
 });
 
